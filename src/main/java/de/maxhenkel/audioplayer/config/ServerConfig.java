a312b003ce869a90bf7425c196416ec80573f0ae
HintSystem <kokins24jul@gmail.com>
2022-12-06 19:04:57 +0000

    Use Forge config builder

--- src/main/java/de/maxhenkel/audioplayer/config/ServerConfig.java
+++ src/main/java/de/maxhenkel/audioplayer/config/ServerConfig.java
@@ -1,40 +1,43 @@
 package de.maxhenkel.audioplayer.config;
 
-import de.maxhenkel.configbuilder.ConfigBuilder;
-import de.maxhenkel.configbuilder.ConfigEntry;
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public class ServerConfig {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
 
-    public final ConfigEntry<String> filebinUrl;
-    public final ConfigEntry<Integer> maxUploadSize;
-    public final ConfigEntry<Integer> uploadPermissionLevel;
-    public final ConfigEntry<Integer> applyToItemPermissionLevel;
-    public final ConfigEntry<Boolean> jukeboxHopperInteraction;
-    public final ConfigEntry<Boolean> jukeboxDispenserInteraction;
-    public final ConfigEntry<Integer> goatHornCooldown;
-    public final ConfigEntry<Double> musicDiscRange;
-    public final ConfigEntry<Double> goatHornRange;
-    public final ConfigEntry<Boolean> allowWavUpload;
-    public final ConfigEntry<Boolean> allowMp3Upload;
-    public final ConfigEntry<Integer> maxMusicDiscDuration;
-    public final ConfigEntry<Integer> maxGoatHornDuration;
-    public final ConfigEntry<Integer> cacheSize;
+    public static final ForgeConfigSpec.ConfigValue<String> filebinUrl;
+    public static final ForgeConfigSpec.ConfigValue<Integer> maxUploadSize;
+    public static final ForgeConfigSpec.ConfigValue<Integer> uploadPermissionLevel;
+    public static final ForgeConfigSpec.ConfigValue<Integer> applyToItemPermissionLevel;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> jukeboxHopperInteraction;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> jukeboxDispenserInteraction;
+    public static final ForgeConfigSpec.ConfigValue<Integer> goatHornCooldown;
+    public static final ForgeConfigSpec.ConfigValue<Double> musicDiscRange;
+    public static final ForgeConfigSpec.ConfigValue<Double> goatHornRange;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> allowWavUpload;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> allowMp3Upload;
+    public static final ForgeConfigSpec.ConfigValue<Integer> maxMusicDiscDuration;
+    public static final ForgeConfigSpec.ConfigValue<Integer> maxGoatHornDuration;
+    public static final ForgeConfigSpec.ConfigValue<Integer> cacheSize;
 
-    public ServerConfig(ConfigBuilder builder) {
-        filebinUrl = builder.stringEntry("filebin_url", "https://filebin.net/");
-        maxUploadSize = builder.integerEntry("max_upload_size", 1000 * 1000 * 20, 1, Integer.MAX_VALUE);
-        uploadPermissionLevel = builder.integerEntry("upload_permission_level", 0, 0, Integer.MAX_VALUE);
-        applyToItemPermissionLevel = builder.integerEntry("apply_to_item_permission_level", 0, 0, Integer.MAX_VALUE);
-        jukeboxHopperInteraction = builder.booleanEntry("jukebox_hopper_interaction", true);
-        jukeboxDispenserInteraction = builder.booleanEntry("jukebox_dispenser_interaction", true);
-        goatHornCooldown = builder.integerEntry("goat_horn_cooldown", 140, 1, Short.MAX_VALUE);
-        musicDiscRange = builder.doubleEntry("music_disc_range", 65D, 1D, Integer.MAX_VALUE);
-        goatHornRange = builder.doubleEntry("goat_horn_range", 256D, 1D, Integer.MAX_VALUE);
-        allowWavUpload = builder.booleanEntry("allow_wav_upload", true);
-        allowMp3Upload = builder.booleanEntry("allow_mp3_upload", true);
-        maxMusicDiscDuration = builder.integerEntry("max_music_disc_duration", 60 * 5, 1, Integer.MAX_VALUE);
-        maxGoatHornDuration = builder.integerEntry("max_goat_horn_duration", 20, 1, Integer.MAX_VALUE);
-        cacheSize = builder.integerEntry("cache_size", 16, 0, Integer.MAX_VALUE);
+    static {
+        filebinUrl = BUILDER.define("filebin_url", "https://filebin.net/");
+        maxUploadSize = BUILDER.defineInRange("max_upload_size", 1000 * 1000 * 20, 1, Integer.MAX_VALUE);
+        uploadPermissionLevel = BUILDER.defineInRange("upload_permission_level", 0, 0, Integer.MAX_VALUE);
+        applyToItemPermissionLevel = BUILDER.defineInRange("apply_to_item_permission_level", 0, 0, Integer.MAX_VALUE);
+        jukeboxHopperInteraction = BUILDER.define("jukebox_hopper_interaction", true);
+        jukeboxDispenserInteraction = BUILDER.define("jukebox_dispenser_interaction", true);
+        goatHornCooldown = BUILDER.defineInRange("goat_horn_cooldown", 140, 1, Short.MAX_VALUE);
+        musicDiscRange = BUILDER.defineInRange("music_disc_range", 65D, 1D, Integer.MAX_VALUE);
+        goatHornRange = BUILDER.defineInRange("goat_horn_range", 256D, 1D, Integer.MAX_VALUE);
+        allowWavUpload = BUILDER.define("allow_wav_upload", true);
+        allowMp3Upload = BUILDER.define("allow_mp3_upload", true);
+        maxMusicDiscDuration = BUILDER.defineInRange("max_music_disc_duration", 60 * 5, 1, Integer.MAX_VALUE);
+        maxGoatHornDuration = BUILDER.defineInRange("max_goat_horn_duration", 20, 1, Integer.MAX_VALUE);
+        cacheSize = BUILDER.defineInRange("cache_size", 16, 0, Integer.MAX_VALUE);
+
+        SPEC = BUILDER.build();
     }
 
 }
